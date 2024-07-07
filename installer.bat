95553086e4cb3aa4e9385351b71ee88767757be9
Dotmini <64796016+XevilA@users.noreply.github.com>
2024-07-08 03:51:07 +0700

    Create installer.bat
    PiC for Windows 10/11

---
+++ installer.bat
@@ -0,0 +1,32 @@
+@echo off
+
+rem Upgrade pip
+python -m pip install --upgrade pip
+
+rem Install packages
+pip install tensorflow
+pip install --upgrade keras
+pip install numpy
+pip install opencv-python
+pip install opencv-contrib-python
+pip install PyQt6
+
+rem Run your Python script (replace script_name.py with your actual Python script)
+python dotmini.py
+
+rem Pause to keep the window open after execution (optional)
+pause
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                
