5dca5234133ff07d6a88da11987a38942371354a
Dotmini <64796016+XevilA@users.noreply.github.com>
2024-07-08 03:50:08 +0700

    Create installer.sh
    PiC for Linux and MacOS !

---
+++ installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python3 -m pip install --upgrade pip
+
+pip install tensorflow
+pip install --upgrade keras
+pip install numpy
+pip install opencv-python
+pip install opencv-contrib-python
+pip install PyQt6
+
+
+python dotmini.py
+
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                                                                                                                                                                    
+~                 
