8b0af79dee66fdb87a1177f2102f53ecb30b9c58
Dotmini Software & Defense <64796016+XevilA@users.noreply.github.com>
2024-07-10 02:07:47 +0700

    Create dotmini_windows.py

---
+++ dotmini_windows.py
@@ -0,0 +1,33 @@
+import time
+import os
+
+class DeveloperProfile:
+    def __init__(self, name, role, company):
+        self.name = name
+        self.role = role
+        self.company = company
+    
+    def present_profile(self):
+        profile = f"Developer Profile:\n\nName: {self.name}\nRole: {self.role}\nCompany: {self.company}"
+        return profile
+
+GREEN = '\033[92m'  
+RESET = '\033[0m'   
+
+def animate_ascii():
+    frames = [
+        GREEN + r"  /| ________________",
+        GREEN + r'O|===|* >________________>',
+        GREEN + r"  \|",
+        RESET
+    ]
+    for frame in frames:
+        os.system('cls' if os.name == 'nt' else 'clear')  # Use 'cls' on Windows, 'clear' on Unix-like
+        print(frame)
+        time.sleep(0.5)
+
+tirawat_profile = DeveloperProfile("Tirawat Nantamas", "CEO and Founder", "Dotmini Software & Defense")
+
+animate_ascii()
+print("\n")
+print(tirawat_profile.present_profile())
